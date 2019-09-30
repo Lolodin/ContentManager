dcce103089a73523830bf4f8f0917fe738636d8f
Nikita Lebedev <ilii4@yandex.ru>
2019-09-30 11:31:29 +0400

    add SaveFile

--- shemeDB.go
+++ shemeDB.go
@@ -1,29 +1,26 @@
 package main
 
 import (
-//"crypto/md5"
 	"database/sql"
-	"fmt"
-
-	//"fmt"
 	_"github.com/go-sql-driver/mysql"
 )
 var(
 	db, _= sql.Open("mysql", "root:root@tcp(localhost:1994)/mydata")
 )
 func main() {
-
-
-//createShemeDB(db)
-
-//result, err := db.Exec("insert into mydata.user (login, password, email) values ('Peeeixel 2', 'Google', 64000)")
-
-
+//createFileDB()
+//createShemeDB()
+//test()
+}
 func createShemeDB () {
 db.Exec("create table user (id int not null auto_increment, login varchar(20) not null,password varchar(20) not null, " +
 	"email varchar(50) not null, primary key(id))")
 }
 func createFileDB()  {
-	db.Exec("create table userFile (id int not null auto_increment, filename varchar(35) not null, userID int) ")
+db.Exec("create table userFile (id int not null auto_increment" +
+	" primary key , filename varchar(35) not null, userID int) ")
 }
-
+func test()  {
+	db.Exec("insert into user(login, password, email) values (?,?,?)", "test", "test", "test@test")
+	db.Exec("insert into userFile(filename, userID) values (?, ?)", "testfile", 1)
+}
